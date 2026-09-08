104c58d386131f95047142ad4058e4d19a6d69f1
khodakovskyvladislav <khodakovsky.vladislav@gmail.com>
2026-09-08 20:29:06 +0300

    replace func

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,7 +1,15 @@
+# def split_integer(value: int, number_of_parts: int) -> list:
+#     parts = []
+#     for parts_left in range(number_of_parts, 0, -1):
+#         next_number = value // parts_left
+#         parts.append(value // parts_left)
+#         value -= next_number
+#     return parts
 def split_integer(value: int, number_of_parts: int) -> list:
     parts = []
-    for parts_left in range(number_of_parts, 0, -1):
+    for parts_left in range(min(value, number_of_parts), 0, -1):
         next_number = value // parts_left
-        parts.append(value // parts_left)
+        parts.append(next_number)
         value -= next_number
+    parts += [0] * (number_of_parts - len(parts))
     return parts
